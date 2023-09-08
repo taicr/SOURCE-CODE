05d23338f3f1c2695d0ddb74c9e9c710642f0851
taicr <taithientam2k4@gmail.com>
2023-09-08 23:28:10 +0700

    update

---
+++ Basic/test.cpp
@@ -0,0 +1,8 @@
+#include<bits/stdc++.h>
+
+using namespace std;
+#define run() ios_base :: sync_with_stdio(false);cin.tie(NULL);cout.tie(NULL);
+#define ll long long
+int main(){
+    cout<<"dsdsd";
+}
