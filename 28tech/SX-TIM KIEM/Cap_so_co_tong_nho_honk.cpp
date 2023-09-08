e2e4915dac08ed6e4d4c440b9856eaa1ea171aae
taicr <taithientam2k4@gmail.com>
2023-09-08 22:44:02 +0700

    commit

---
+++ 28tech/SX-TIM KIEM/Cap_so_co_tong_nho_honk.cpp
@@ -0,0 +1,54 @@
+#include<bits/stdc++.h>
+using namespace std;
+int a[1000005];
+//Cap so co tong<k;
+/*int last_post(int a[],int l,int r,int x){
+	int res=-1;
+	while(l<=r){
+		int m=(l+r)/2;
+		if(a[m]<x){
+			res=m;
+			l=m+1;
+		}
+		else r=m-1;
+	}
+	return res;
+}
+int main(){
+	int n,k;
+	cin>>n>>k;
+	for(int i=0;i<n;i++) cin>>a[i];
+	sort(a,a+n);
+	long long ans=0;
+	for(int i=0;i<n;i++){
+		int l=last_post(a,i+1,n-1,k-a[i]);
+		if(l!=-1) ans+=l-i;
+	}
+	cout<<ans;
+}
+*/
+//cap so co tong>k: 
+int begin_post(int a[],int l,int r,int x){
+	int res=-1;
+	while(l<=r){
+		int m=(l+r)/2;
+		if(a[m]>x){
+			res=m;
+			r=m-1;
+		}
+		else l=m+1;
+	}
+	return res;
+}
+int main(){
+	int n,k;
+	cin>>n>>k;
+	for(int i=0;i<n;i++) cin>>a[i];
+	sort(a,a+n);
+	long long ans=0;
+	for(int i=0;i<n;i++){
+		int l=begin_post(a,i+1,n-1,k-a[i]);
+		if(l!=-1) ans+=n-l;
+	}
+	cout<<ans;
+}
